e70ceac464e3d75946173d862a67260cad992011
1kreidie <karnikova.vika@mail.ru>
2025-03-04 12:28:20 +0300

    adding table and header

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          { content: 'ФИО' },
+          { content: 'ФИО' },
+          { content: 'ФИО' },
+          { content: 'ФИО' },
+          { content: 'ФИО' },
+          { content: 'ФИО' },
+          { content: 'ФИО' },
+          { content: 'ФИО' },
+          { content: 'ФИО' },
+        ]
+      }
+    },
+  }
+</script>
+
+<template>
+  <div>
+    <table class="minimalistBlack">
+<thead>
+<tr>
+<th v-for="item in items" :key="item">{{ item.content }}</th>
+</tr>
+</thead>
+
+<tbody>
+<tr>
+<td>cell1_1</td>
+<td>cell2_1</td>
+<td>cell3_1</td>
+<td>cell4_1</td>
+<td>cell5_1</td>
+<td>cell6_1</td>
+<td>cell7_1</td>
+<td>cell8_1</td>
+<td>cell9_1</td>
+</tr>
+</tbody>
+</table>
+  </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
